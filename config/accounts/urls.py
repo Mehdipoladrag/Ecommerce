05ed07876b10d95119a6ce2459b6d9f4f5ebec77
Mehdipoladrag <mehdipoladrag1382@gmail.com>
2024-04-23 16:07:29 +0330

    create Registration form & verify Code (7)

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from accounts.views import UserRegisterView
+from accounts.views import UserRegisterView,UserRegisterVerifyCode
 app_name = 'accounts' 
 
 urlpatterns = [
   path('register/', UserRegisterView.as_view(), name='user_register'),
+    path('verify/', UserRegisterVerifyCode.as_view(), name='verify_code'),
 ]
