c5dba43a8ed96e35a212f17f0c901669b305bb1e
Mehdipoladrag <mehdipoladrag1382@gmail.com>
2024-04-23 15:36:57 +0330

    create Registration form (6)

--- config/accounts/admin.py
+++ config/accounts/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin 
 from accounts.forms import UserCreationForm, UserChangeForm
-from accounts.models import User 
+from accounts.models import User, OtpCode
 from django.contrib.auth.models import Group
 # Register your models here.
+
+
+@admin.register(OtpCode)
+class OtpCodeAdmin(admin.ModelAdmin) :
+  list_display = ['phone_number', 'code', 'created'
+]
 
 class UserAdmin(BaseUserAdmin) : 
   form = UserChangeForm 
---
+++ config/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+from accounts.views import UserRegisterView
+app_name = 'accounts' 
+
+urlpatterns = [
+  path('register/', UserRegisterView.as_view(), name='user_register'),
+]
