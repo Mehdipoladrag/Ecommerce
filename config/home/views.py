972d3496ee864464d67bb40aaa1898c6d0ba380f
Mehdipoladrag <mehdipoladrag1382@gmail.com>
2024-05-21 14:07:15 +0330

    Create Task && Buckets

--- config/home/views.py
+++ config/home/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Product
-
+from . import tasks
+from django.contrib import messages
 
 # Create your views here.
 
@@ -19,3 +20,28 @@ class ProductDetailView(View):
         product = get_object_or_404(Product, slug=slug)
         return render(request, 'home/detail.html', {'product': product})
 
+
+
+
+
+class BucketHome(View):
+    template_name = 'home/bucket.html'
+
+    def get(self, request):
+        objects = tasks.all_bucket_objects_task()
+        return render(request, self.template_name, {'objects':objects})
+
+
+class DeleteBucketObject(View):
+    def get(self, request, key):
+        tasks.delete_object_task.delay(key)
+        messages.success(request, 'your object will be delete soon.', 'info')
+        return redirect('home:bucket')
+
+
+class DownloadBucketObject(View):
+
+    def get(self, request, key):
+        tasks.download_object_task.delay(key)
+        messages.success(request, 'your download will start soon.', 'info')
+        return redirect('home:bucket')
