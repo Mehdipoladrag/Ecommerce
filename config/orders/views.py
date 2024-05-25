d85b844b319ccf27888409986813e778508923b1
Mehdi Poladrag <mehdipoladrag1382@example.com>
2024-05-26 01:48:38 +0330

    Create remove For cart

--- config/orders/views.py
+++ config/orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .cart import Cart
 from .forms import CartAddForm
-from ..home.models import Product
+from home.models import Product
 
 
 # Create your views here.
@@ -10,7 +10,8 @@ from ..home.models import Product
 
 class CartView(View):
     def get(self, request):
-        return render(request, 'orders/cart.html')
+        cart = Cart(request)
+        return render(request, 'orders/cart.html', {'cart':cart})
 
 
 class CartAddView(View):
@@ -20,4 +21,12 @@ class CartAddView(View):
         form = CartAddForm(request.POST)
         if form.is_valid():
             cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
+
+
+class CartRemoveView(View):
+    def get(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
         return redirect('orders:cart')
